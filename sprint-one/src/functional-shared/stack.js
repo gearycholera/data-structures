6b770ed1d8f333cee15e36d277e1adc7c17e20fb
girikalra <girikalra@gmail.com>
2018-01-17 14:58:07 -0800

    passes all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,16 +3,24 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
   someInstance.count = 0;
+  someInstance.storage = {};
   extend(someInstance, stackMethods);
   return someInstance;
 };
 
 var stackMethods = {
-  push: function() {
-    return this.count++;
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
   },
   pop: function() {
-    return this.count--;
+    var popped = this.storage[this.count - 1];
+    delete this.storage[this.count];
+    this.count--;
+    if (this.count < 0) {
+      this.count = 0;
+    }
+    return popped;
   },
   size: function() {
     return this.count;
