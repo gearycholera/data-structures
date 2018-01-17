063f2c6505d780fe12050e816d163aa75ece6dd5
girikalra <girikalra@gmail.com>
2018-01-17 15:05:26 -0800

    passes first test

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  extend(someInstance, queueMethods);
+  someInstance.count = 0;
+  someInstance.lowestCount = 0;
+  someInstance.magnitude = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.magnitude;
+  }
+};
 
-
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
