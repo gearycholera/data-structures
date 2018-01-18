ccea22ae288d0a7eb762487f57bc099c878910eb
girikalra <girikalra@gmail.com>
2018-01-18 11:40:00 -0800

    added time complexity

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -74,4 +74,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  // addToTail : O(1) constant time
+  // removeHead : O(1) constant time
+  // contains : O(n) linear time
  */
