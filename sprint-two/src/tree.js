0571ded095407d8e2548c001ac1293c25d02f33d
girikalra <girikalra@gmail.com>
2018-01-18 11:40:42 -0800

    passes all tests and time complexity

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,38 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tempTree = new Tree(value);
+  this.children.push(tempTree);
+  
 };
 
 treeMethods.contains = function(target) {
+  var isFound = false;
+  var recursiveContains = function(tree) {
+    if (tree.value === target) {
+      isFound = true;
+    } else if (tree.children.length > 0) {
+      for (var i = 0; i < tree.children.length; i++) {
+        recursiveContains(tree.children[i]);
+      }
+    }
+  };
+  recursiveContains(this);
+  return isFound;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  // addChild = O(1) - constant
+  // contains = O(n) - linear
  */
