00308f3167908050e12581151c330d56bbb02c98
girikalra <girikalra@gmail.com>
2018-01-18 20:04:37 -0800

    partially working. need to add more

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,26 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.keys = {};
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this.keys[index] = k;
+  // start here
+  if (this.keys[index]) {
+    this._storage[index] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[index];
 };
 
 
